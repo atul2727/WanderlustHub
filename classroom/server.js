00b57ccedf6d0fb61bb0c70fe1b0ca54c2967295
atul2727 <atuljaat2004aj@gmail.com>
2024-04-14 18:04:43 +0530

    learning about cookies

--- classroom/server.js
+++ classroom/server.js
@@ -1,8 +1,34 @@
 const express = require("express")
 const app = express()
+const cookieParser = require("cookie-parser")
 
-app.listen(2700,(req,res)=>{
-    console.log("Server is running on port 2700")
+app.use(cookieParser("secret code"))
+
+app.get("/signedcookies", (req, res)=>{
+    res.cookie("signedname","atul", {signed: true})
+    res.send("signed cookie set")
+})
+
+app.get("/verify", (req, res)=>{
+    console.log(req.cookies)            // unsigned cookies
+    console.log(req.signedCookies)      // signed cookies
+    res.send(req.signedCookies)
+})
+
+app.get("/getcookies",(req,res)=>{
+    res.cookie("name","atul")
+    res.cookie("name2","arun")
+    res.send("cookies set")
+})
+
+app.get("/greet",(req,res)=>{
+    let {name = "user"} = req.cookies;
+    res.send(`Hello ${name}`)
+})
+
+app.get("/", (req, res)=>{
+    console.dir(req.cookies)
+    res.send("I am root");
 })
 
 const users = require("./routes/user.js")
@@ -15,4 +41,9 @@ app.use("/", posts)
 
 app.get("/", (req, res)=>{
     res.send("I am root");
+})
+
+
+app.listen(2700,(req,res)=>{
+    console.log("Server is running on port 2700")
 })
