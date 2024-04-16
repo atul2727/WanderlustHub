aa293438cb053ff9bb3a18df59c07fe574254d65
atul2727 <atuljaat2004aj@gmail.com>
2024-04-16 22:15:26 +0530

    learnt about connect-flash

--- classroom/server.js
+++ classroom/server.js
@@ -3,6 +3,12 @@ const app = express()
 const users = require("./routes/user.js")
 const posts = require("./routes/post.js")
 const session = require("express-session")
+const flash = require("connect-flash")
+
+const path = require("path");
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 const sessionOptions = ({
     secret: "secretstring",
     resave: false,
@@ -10,15 +16,30 @@ const sessionOptions = ({
 })
 
 app.use(session(sessionOptions));
+app.use(flash());
 
-app.get("/register", (req, res)=>{
-    let {name = "Anonymous", age} = req.query;
+app.use((req, res, next) => {
+    res.locals.successMessages = req.flash("success");
+    res.locals.errorMessages = req.flash("error");
+    next();
+});
+
+app.get("/register", (req, res) => {
+    let { name = "anonymous", age } = req.query;
     req.session.name = name;
+    // req.flash("key", "this is a sample message")
+    if (name === "anonymous") {
+        req.flash("error", "some error")
+    } else {
+        req.flash("succcess", "success!")
+    }
     // res.send(`Welcome ${name} your age is ${age}`);
     res.redirect("/hello");
 })
-app.get("/hello", (req,res)=>{
-    res.send(`Hello ${req.session.name}`);
+app.get("/hello", (req, res) => {
+    // res.send(`Hello ${req.session.name}`);
+    // console.log(res.locals.messages)
+    res.render("page.ejs", { name: req.session.name });
 })
 
 app.get("/test", (req, res) => {
@@ -26,9 +47,9 @@ app.get("/test", (req, res) => {
 })
 app.get("/reqcount", (req, res) => {
     if (req.session.count) {
-        req.session.count ++
+        req.session.count++
     } else {
-        req.session.count = 1 
+        req.session.count = 1
     }
     res.send(`You have visited this page ${req.session.count} times`)
 })
